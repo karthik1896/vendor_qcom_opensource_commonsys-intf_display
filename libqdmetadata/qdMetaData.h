172bf8aa75f325575008f9c04ff3f6e30a6b13ba
Dhruv <dhruvgera61@gmail.com>
2022-04-04 15:42:58 +0200

    commonsys-intf: Add missing declaration

--- libqdmetadata/qdMetaData.h
+++ libqdmetadata/qdMetaData.h
@@ -60,6 +60,13 @@ enum ColorSpace_t {
   ITU_R_709_FR,
 };
 
+struct HSICData_t {
+    int32_t hue;
+    float   saturation;
+    int32_t intensity;
+    float   contrast;
+};
+
 struct BufferDim_t {
   int32_t sliceWidth;
   int32_t sliceHeight;
